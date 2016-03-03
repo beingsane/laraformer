b1453fa6f2b2eba750d5aff13ba38d1c92b9f1e0
Kamran Ahmed <kamran.ahmed@tajawal.com>
2016-03-03 19:20:45 +0500

    Basic skeleton for the transformer

---
+++ src/Middewares/TransformerMiddleware.php
@@ -0,0 +1,42 @@
+<?php namespace KamranAhmed\Middlewares\LaraFormer;
+
+use Closure;
+use KamranAhmed\LaraFormer\Transformer;
+
+/**
+ * Class TransformerMiddleware
+ *
+ * Middleware that tries to automatically transform the data provided
+ * that the direct model
+ */
+class TransformerMiddleware
+{
+    public function __construct(Transformer $transformer)
+    {
+        $this->transformer = $transformer;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        // Having the `original` property means that we have the models and
+        // the response can be tried to transform
+        if (property_exists($response, 'original')) {
+            // Transform based on model and reset the content
+            $content = $this->transformer->transformModel($response->original);
+            $response->setContent($content);
+        }
+
+        return $response;
+    }
+}
+
---
+++ src/Transformer.php
@@ -0,0 +1,14 @@
+<?php namespace KamranAhmed\LaraFormer;
+
+class Transformer
+{
+    public function __construct()
+    {
+
+    }
+
+    public function transformModel()
+    {
+
+    }
+}
